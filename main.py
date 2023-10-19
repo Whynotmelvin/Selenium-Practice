eafaf1d7bfead094f052ca924eeca6ba76ce1c08
Melvin <melvinundercover@icloud.com>
2023-10-19 23:40:01 +0100

    Remote driver

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Replace these with your actual Instagram username and password
+username = "Melvin.aj"
+password = "Sophia2.0"
+
+driver = webdriver.Chrome()
+
+# Open Instagram
+driver.get("https://www.instagram.com/")
 
 
-browser = webdriver.Chrome()
-browser.get('https://www.amazon.com/s?k=dyson+hair+dryer&i=stripbooks-intl-ship&crid=29H1MBADOCP5V&sprefix=dyson%2Cstripbooks-intl-ship%2C465&ref=nb_sb_ss_ts-doa-p_3_5')
-elem_list = browser.find_element(By.CSS_SELECTOR,"div.s-main-slot.s-result-list.s-search-results.sg-row")
+# Find the username and password fields and fill them in
+username_field = driver.find_element_by_name("username")
+password_field = driver.find_element_by_name("password")
 
-items =elem_list.find_elements(By.XPATH,'//div[@data-component-type="s-search-result"]')
-print(len(items))
-elem = browser.find_element
+username_field.send_keys(username)
+password_field.send_keys(password)
+
+# Submit the form
+password_field.send_keys(Keys.RETURN)
+
+# Wait for some time to see the result (replace with actual wait strategies)
+time.sleep(5)
+
+# Close the browser
+driver.quit()
